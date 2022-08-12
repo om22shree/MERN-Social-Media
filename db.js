032b6e9109eae208c18631f91072eba2da73ca39
Om Shree <om22shree@gmail.com>
2022-08-12 10:02:01 +0530

    db updated

--- db.js
+++ db.js
@@ -1,12 +1,14 @@
-const {MongoClient} = require('mongodb')
+const { MongoClient } = require("mongodb");
 
-const client = new MongoClient('mongodb+srv://username:password@cluster0.dfvvi.mongodb.net/TodoApp?retryWrites=true&w=majority')
+const client = new MongoClient(
+  "mongodb+srv://om:2002@cluster0.dfvvi.mongodb.net/ComplexApp?retryWrites=true&w=majority"
+);
 
 async function start() {
-  await client.connect()
-  module.exports = client.db()
-  const app = require('./app')
-  app.listen(3000)
+  await client.connect();
+  module.exports = client.db();
+  const app = require("./app");
+  app.listen(3000);
 }
 
-start()
+start();
