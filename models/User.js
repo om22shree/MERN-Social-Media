6dba91d2d2ae79cfd0f0f764223eb5a8c0b7f3a9
Om Shree <om22shree@gmail.com>
2022-08-14 01:29:38 +0530

    login promise

--- models/User.js
+++ models/User.js
@@ -70,16 +70,18 @@ User.prototype.register = function () {
 User.prototype.login = function () {
   return new Promise((resolve, reject) => {
     this.cleanUp();
-    usersCollection.findOne(
-      { username: this.data.username },
-      (err, attemptedUser) => {
+    usersCollection
+      .findOne({ username: this.data.username })
+      .then((attemptedUser) => {
         if (attemptedUser && attemptedUser.password == this.data.password) {
           resolve("Congrats");
         } else {
           reject("Invalid username/password");
         }
-      }
-    );
+      })
+      .catch(function () {
+        reject("Please try again");
+      });
   });
 };
 
